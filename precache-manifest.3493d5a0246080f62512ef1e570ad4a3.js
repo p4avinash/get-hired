244041cdec171359fd22ccd8ce8dfd867dc4b8ff
Avinash <p4avinashmaurya@gmail.com>
2020-07-24 17:05:13 +0530

    Updates

---
+++ precache-manifest.3493d5a0246080f62512ef1e570ad4a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e109604887dcaffe27790e39b8c20d3",
+    "url": "/get-hired.git/index.html"
+  },
+  {
+    "revision": "2e1e9b02313255601f22",
+    "url": "/get-hired.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e00af23441a08910a0fa",
+    "url": "/get-hired.git/static/css/main.636ad754.chunk.css"
+  },
+  {
+    "revision": "2e1e9b02313255601f22",
+    "url": "/get-hired.git/static/js/2.09347750.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/get-hired.git/static/js/2.09347750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00af23441a08910a0fa",
+    "url": "/get-hired.git/static/js/main.034ece85.chunk.js"
+  },
+  {
+    "revision": "cde88d575d015e161a3c",
+    "url": "/get-hired.git/static/js/runtime-main.32a2411f.js"
+  }
+]);
